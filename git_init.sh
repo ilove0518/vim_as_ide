942aedd10b441b4da5b827975a0e0f60d2a2f0f4
ilove0518 <ilove0518@sina.com>
2018-02-19 06:59:22 -0800

    frist submit

---
+++ git_init.sh
@@ -0,0 +1,6 @@
+echo "# vim_as_ide" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/ilove0518/vim_as_ide.git
+git push -u origin master
